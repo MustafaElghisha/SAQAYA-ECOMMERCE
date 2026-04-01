05cda4554ea2fe3bfcd51ea46acbd11919d3871d
MustafaElghisha <mustafaelghisha@gmail.com>
2026-04-01 16:09:58 +0200

    chore(assets): add local font files

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 // import router from "./router";
 // import store from "./store";
 
-import "./assets/styles/normalize.css";
+import "./assets/styles/global.css";
 
 Vue.config.productionTip = false;
 
