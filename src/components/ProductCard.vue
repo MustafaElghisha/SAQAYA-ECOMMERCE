1226d937a8f19ab3e825b22aa02be4eb3bf63e9a
MustafaElghisha <mustafaelghisha@gmail.com>
2026-04-02 16:08:52 +0200

    Add ProductCard component for product display

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <li class="product-card__item">
+    <div class="product-card__product">
+      <img
+        src="../assets/images/HAVIT HV-G92 Gamepad.png"
+        alt="HAVIT HV-G92 Gamepad"
+        class="product-card__product-image"
+      />
+      <img
+        src="../assets/icons/heart.svg"
+        alt=""
+        width="24"
+        height="24"
+        class="product-card__product-fav"
+      />
+      <img
+        src="../assets/icons/Quick View.svg"
+        alt=""
+        width="24"
+        height="24"
+        class="product-card__product-view"
+      />
+    </div>
+    <h3 class="product-card__product-title">HAVIT HV-G92 Gamepad</h3>
+    <div class="product-card__pricing">
+      <span class="product-card__current-price">$120</span>
+      <span class="product-card__original-price">$160</span>
+    </div>
+    <div class="product-card__rating">
+      <span class="product-card__stars">
+        <img
+          src="../assets/icons/Five star.svg"
+          alt="Rating stars"
+          class="product-card__star-icon"
+        />
+      </span>
+      <span class="product-card__review-count">(88)</span>
+    </div>
+  </li>
+</template>
+
+<style lang="css" scoped>
+.product-card__product {
+  background-color: #fafafa;
+  padding: 40px;
+  position: relative;
+  margin-bottom: 16px;
+  border-radius: 4px;
+}
+.product-card__product-fav {
+  top: 12px;
+  right: 12px;
+}
+.product-card__product-view {
+  top: 54px;
+  right: 12px;
+}
+.product-card__product-fav,
+.product-card__product-view {
+  width: 36px;
+  height: 36px;
+  position: absolute;
+  padding: 5px;
+  background-color: white;
+  border-radius: 50%;
+}
+.product-card__product-title,
+.product-card__original-price,
+.product-card__current-price {
+  font-weight: 500;
+}
+.product-card__pricing {
+  padding-block: 8px;
+  display: flex;
+  gap: 12px;
+}
+.product-card__rating {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+}
+.product-card__review-count {
+  font-weight: 600;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.5);
+}
+.product-card__current-price {
+  color: #db4444;
+}
+.product-card__original-price {
+  color: rgba(0, 0, 0, 0.5);
+  text-decoration-line: line-through;
+}
+</style>
