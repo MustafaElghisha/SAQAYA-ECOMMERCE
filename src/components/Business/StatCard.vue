183b91ad100d9e34f66bb8e0b1a6ccaa37f75939
MustafaElghisha <mustafaelghisha@gmail.com>
2026-04-11 19:19:05 +0200

    refactor(about): extract StatCard componen

---
+++ src/components/Business/StatCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="about__stat-card">
+    <div class="about__stat-icon-wrapper">
+      <img class="about__stat-icon" :src="stat.icon" :alt="stat.alt" />
+    </div>
+    <span class="about__stat-number">{{ stat.number }}</span>
+    <p class="about__stat-label">{{ stat.label }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["stat"],
+};
+</script>
+
+<style lang="css" scoped>
+.about__stat-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  cursor: pointer;
+  padding: 2rem 2.25rem;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 0.25rem;
+  transition: all 200ms ease-in-out;
+
+  &:hover {
+    color: white;
+    background-color: var(--clr-primary-500);
+    border-color: transparent;
+
+    .about__stat-icon-wrapper {
+      background-color: white;
+      outline-color: rgba(255, 255, 255, 0.3);
+
+      > img {
+        filter: invert(100%);
+        transition: all 200ms ease-in-out;
+      }
+    }
+  }
+}
+.about__stat-icon-wrapper {
+  padding: 0.625rem;
+  background-color: black;
+  border-radius: 50%;
+  outline: 12px solid rgba(47, 46, 48, 0.3);
+  transition: all 200ms ease-in-out;
+}
+.about__stat-number {
+  font-size: var(--fs-2xl);
+  font-weight: 700;
+  padding-block: 1.5rem 0.75rem;
+}
+</style>
--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -24,13 +24,7 @@
     </section>
 
     <section class="section about__stats">
-      <div class="about__stat-card" v-for="(stat, index) in stats" :key="index">
-        <div class="about__stat-icon-wrapper">
-          <img class="about__stat-icon" :src="stat.icon" :alt="stat.alt" />
-        </div>
-        <span class="about__stat-number">{{ stat.number }}</span>
-        <p class="about__stat-label">{{ stat.label }}</p>
-      </div>
+      <StatCard v-for="(stat, index) in stats" :key="index" :stat="stat" />
     </section>
 
     <section class="section about__team">
@@ -98,45 +92,6 @@
   gap: 1.875rem;
   justify-content: center;
 }
-.about__stat-card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  cursor: pointer;
-  padding: 2rem 2.25rem;
-  border: 1px solid rgba(0, 0, 0, 0.3);
-  border-radius: 0.25rem;
-  transition: all 200ms ease-in-out;
-
-  &:hover {
-    color: white;
-    background-color: var(--clr-primary-500);
-    border-color: transparent;
-
-    .about__stat-icon-wrapper {
-      background-color: white;
-      outline-color: rgba(255, 255, 255, 0.3);
-
-      > img {
-        filter: invert(100%);
-        transition: all 200ms ease-in-out;
-      }
-    }
-  }
-}
-.about__stat-icon-wrapper {
-  padding: 0.625rem;
-  background-color: black;
-  border-radius: 50%;
-  outline: 12px solid rgba(47, 46, 48, 0.3);
-  transition: all 200ms ease-in-out;
-}
-.about__stat-number {
-  font-size: var(--fs-2xl);
-  font-weight: 700;
-  padding-block: 1.5rem 0.75rem;
-}
 
 .about__team {
   display: grid;
@@ -171,10 +126,12 @@ import shoppingBagIcon from "@/assets/icons/Shopping bag.svg";
 import moneyBagIcon from "@/assets/icons/Moneybag.svg";
 
 import FeaturesList from "@/components/Business/FeaturesList.vue";
+import StatCard from "@/components/Business/StatCard.vue";
 
 export default {
   components: {
     FeaturesList,
+    StatCard,
   },
   data() {
     return {
