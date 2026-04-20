1e2f94c195f8e668a47099b83c8fdf54b8d75411
MustafaElghisha <mustafaelghisha@gmail.com>
2026-04-20 18:14:45 +0200

    chore: add Pinia dependency and fix ESLint/Prettier + Vue config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,20 @@
 module.exports = {
   root: true,
-
   env: {
     node: true,
   },
-
   extends: [
-    "plugin:vue/essential",
+    "plugin:vue/vue3-essential",
     "eslint:recommended",
     "plugin:prettier/recommended",
   ],
-
   parserOptions: {
-    ecmaVersion: 2020,
+    parser: "@babel/eslint-parser",
   },
-
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
-
   overrides: [
     {
       files: [
